c67ac0d38751b5d7189e4d7bba5daba4bd7aad9c
Lucas Ferreira Pinto <Lucas.Ferreira.Pinto1@ibm.com>
2020-03-26 00:33:25 -0300

    CRUD Completed

--- index.js
+++ index.js
@@ -1,8 +1,53 @@
 const express = require('express');
 const server = express();
 
+server.use(express.json());
+const geeks = [];
+
 server.get('/geeks', (req, res) => {
-    return res.json({ message: 'Hello word'});
+    return res.json(geeks);
 })
+
+server.get('/geeks/:index', checkUserInArray, (req, res) => {
+    return res.json(req.user)
+;})
+
+function checkGeekExists(req, res, next){
+    if(!req.body.name){
+        return res.status(400).json({ error: 'geek name is required' });
+
+    }
+    return next();
+}
+
+function checkUserInArray(req, res, next){
+    const geek = geeks[req.params.index];
+    if(!geek){
+        return res.status(400).json({ error: 'geek does not exist'});
+    }
+    req.geek = geek;
+    return next();
+}
+
+
+server.post('/geeks', checkGeekExists, (req, res) => {
+    const { name } = req.body;
+    geeks.push(name);
+    return res.json(geeks);
+})
+
+server.put('geeks/:index', checkUserInArray, checkGeekExists, (req, res) => {
+    const { index } = req.params;
+    const { name } = req.body;
+    geeks[index] = name;
+    return res.json(geeks);
+})
+
+server.delete('/geeks/:index', (req, res) => {
+    const { index } = req.params
+    geeks.splice(index, 1);
+    return res.send();
+})
+
 
 server.listen(3000);
